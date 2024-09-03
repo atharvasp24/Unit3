ea85a93813e7bb4469ef1de47cfc52d0c56fba84
Atharva Patil <atharvasp24@gmail.com>
2024-09-03 13:35:42 -0400

    Printed without user input, code working

---
+++ activities/EVENorODD.java
@@ -0,0 +1,15 @@
+package activities;
+
+public class EVENorODD {
+    public static void main(String[] args) {
+        float x = 4;
+
+        if (x%2==0){
+            System.out.println("The numder is even");
+        }
+        
+
+        
+    }
+
+}
